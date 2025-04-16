3ce6f972880f4b72b22d9686b853cf1e6360421d
Tufail Ahmad <tufailahmadashrafi@gmail.com>
2025-04-16 22:07:57 +0530

    Project 2 Bharat-clock-version-one completed

---
+++ Projects/2-bharat-clock-version-one/src/App.jsx
@@ -0,0 +1,17 @@
+import ClockHeading from "./components/ClockHeading";
+import ClockSlogan from "./components/ClockSlogan";
+import CurrentTime from "./components/CurrentTime";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <center>
+      <ClockHeading></ClockHeading>
+      <ClockSlogan></ClockSlogan>
+      <CurrentTime></CurrentTime>
+    </center>
+  );
+}
+
+export default App;
---
+++ Projects/2-bharat-clock-version-one/src/components/ClockSlogan.jsx
@@ -0,0 +1,9 @@
+const ClockSlogan = () => {
+  return (
+    <p className="lead">
+      This the clock that shows the time in Bharat at all times.
+    </p>
+  );
+};
+
+export default ClockSlogan;
