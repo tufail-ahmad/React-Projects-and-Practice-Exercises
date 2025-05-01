d6e3538ea253ee72debe9bb5e47e22badee24e5d
Tufail Ahmad <tufailahmadashrafi@gmail.com>
2025-05-01 18:46:52 +0530

    update

--- Projects/7-social-media/src/App.jsx
+++ Projects/7-social-media/src/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
 
 function App() {
   return (
-    <>
-      <div>Placeholder</div>
-    </>
+    <div className="app-container">
+      <Sidebar></Sidebar>
+      <div className="main-content">
+        <Header></Header>
+        <Main></Main>
+        <Footer></Footer>
+      </div>
+    </div>
   );
 }
 
